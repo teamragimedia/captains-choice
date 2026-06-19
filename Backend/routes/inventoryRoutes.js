6e01c048b9a9b70194bbc892b5ad85be7274ea26
Manikandan.R <teamragimedia@gmail.com>
2026-06-19 13:33:32 +0530

    Fix inventory controller filename case

--- Backend/routes/inventoryRoutes.js
+++ Backend/routes/inventoryRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {
   getInventory,
   addInventoryMovement,
-} = require("../controllers/inventoryController");
+} = require("../controllers/inventorycontroller");
 
 router.get("/", getInventory);
 
