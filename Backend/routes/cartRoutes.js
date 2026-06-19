bd5cf08b43d955120dbac55dce83584578052baf
Manikandan.R <teamragimedia@gmail.com>
2026-06-19 13:35:56 +0530

    fix auth

--- Backend/routes/cartRoutes.js
+++ Backend/routes/cartRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../middleware/auth");
+const authMiddleware = require("../middleware/Auth");
 
 const {
   getCart,
