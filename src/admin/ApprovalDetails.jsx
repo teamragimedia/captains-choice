446093866120f6b61be1eb643484046dbf7399fd
Manikandan.R <teamragimedia@gmail.com>
2026-06-19 11:11:16 +0530

    added A

--- src/admin/ApprovalDetails.jsx
+++ src/admin/ApprovalDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { useParams, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import "./admin.css";
+import "./Admin.css";
 import API from "../api";
 
 const ApprovalDetails = () => {
